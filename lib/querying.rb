da38160c9e8d27f5c2ecfa9c22044d449fb6b898
andrew <neon.duncan@gmail.com>
2021-08-13 13:48:41 -0500

    commit tree ed9118683196cc0ab4ef8980779a354d8921f24b

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,22 +1,32 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+  WHERE series_id = 1
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters
+  GROUP BY name
+  HAVING MAX(LENGTH(motto));"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) AS number FROM characters
+  GROUP BY characters.species
+  HAVING MAX(number);"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT  authors.name, subgenres.name FROM series
+  LEFT JOIN subgenres ON series.subgenre_id = subgenres.id
+  INNER JOIN authors ON authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  GROUP BY series.title
+  HAVING MAX(COUNT(characters.name));"
 end
 
 def select_character_names_and_number_of_books_they_are_in
